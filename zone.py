e403960f216b9f2a53a0bb9a33b3cb9c196d72a7
Antonio Manuel Carvalho Pinheiro <antmanue@c1r11s2.42porto.com>
2026-06-29 16:17:09 +0100

    feat: zone.py first step

--- zone.py
+++ zone.py
@@ -1,1 +1,15 @@
- 
+class ZoneType(Enum):
+	NORMAL = "normal"
+	BLOCKED = "blocked"
+	RESTRICTED = "restricted"
+	PRIORITY = "priority"
+
+
+class Zone():
+	def __init__(self, name: str, coordinates: tuple[int, int], type: ZoneType, color: str, max_drones: int) -> None:
+		self.name: str = name
+		self.coordinates: tuple[int, int] = coordinates
+		self.type: ZoneType = type
+		self.max_drones: int = max_drones
+		self.color: str = color
+		self.capacity_counter: int = 0
